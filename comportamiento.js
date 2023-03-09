c6081ad8053e388cb2df62b7e06746c941c256d2
xXWhisperXx <126434297+xXWhisperXx@users.noreply.github.com>
2023-03-08 23:51:10 -0300

    Add files via upload

--- comportamiento.js
+++ comportamiento.js
@@ -13,7 +13,7 @@ function mostrarOcultarMenu(){
 }
 // ocultar menu una vez que seleccionamos una opcion
 let links = document.querySelectorAll("nav a");
-for(var x = 0; x,links.length; x++){
+for(var x = 0; x<links.length; x++){
     links[x].onclick = function(){
         menu.style.display = "none";
         menu_visible = false;
@@ -36,7 +36,51 @@ let wordpress = document.getElementById("wordpress");
 crearBarra(wordpress);
 let photoshop = document.getElementById("photoshop");
 crearBarra(photoshop);
-let php = document.getElementById("php");
-crearBarra(php);
-let ilustrator = document.getElementById("ilustrator");
-crearBarra(ilustrator);
+let python = document.getElementById("python");
+crearBarra(python);
+let ingles = document.getElementById("ingles");
+crearBarra(ingles);
+
+let contadores = [-1,-1,-1,-1,-1,-1];
+let entro = false;
+function efectoHabilidades(){
+    var habilidades = document.getElementById("habilidades");
+    var distancia_skills = window.innerHeight - habilidades.getBoundingClientRect().top;
+    if(distancia_skills>=300 &&entro == false){
+        entro = true;
+        const intervalHtml = setInterval(function(){
+            pintarBarra(html, 10, 0, intervalHtml);
+        },100);
+        const intervalJavascript = setInterval(function(){
+            pintarBarra(javascript, 9, 1, intervalJavascript);
+        },100);
+        const intervalIngles = setInterval(function(){
+            pintarBarra(ingles, 16, 2, intervalIngles);
+        },100);
+        const intervalWordpress = setInterval(function(){
+            pintarBarra(wordpress, 7, 3, intervalWordpress);
+        },100);
+        const intervalPython = setInterval(function(){
+            pintarBarra(python, 6, 4, intervalPython);
+        },100);
+        const intervalPhotoshop = setInterval(function(){
+            pintarBarra(photoshop, 11, 5, intervalPhotoshop);
+        },100);
+    }
+}
+
+function pintarBarra(id_barra, cantidad, indice, interval){
+    contadores[indice]++;
+    x=contadores[indice];
+    if(x < cantidad){
+        let elementos = id_barra.getElementsByClassName("e");
+        elementos[x].style.backgroundColor = "#940253";
+    }
+    else{
+        clearInterval(interval)
+    }
+}
+
+window.onscroll = function(){
+    efectoHabilidades();
+}
